e76ba3ed7a80c81ccb211aa26049884a1ba54c45
tamaki.kyosuke <tamaki.kyosuke@tamaki>
2017-06-21 09:50:54 +0900

    Git　Example

---
+++ github-example/src/jp/co/comnic/javalesson/github/GithubExample.java
@@ -0,0 +1,11 @@
+package jp.co.comnic.javalesson.github;
+
+public class GithubExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello, Git!");
+	}
+
+}
